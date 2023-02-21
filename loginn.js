c5fba3e4a5102bdc4f0998ee0faff9f820864916
srikanthgunj <48724588+srikanthgunj@users.noreply.github.com>
2023-02-21 21:37:21 +0530

    Create loginn.js

---
+++ loginn.js
@@ -0,0 +1,18 @@
+let  userName=document.querySelector("#user")
+
+let  password=document.querySelector("#pass")
+let button=document.querySelector("#login")
+let wrong=document.querySelector("#wrong")
+ button.onclick=logi
+function logi(){
+   let x=userName.value
+   let y=password.value
+   if ( x=='srikanth' && y =='1234')
+   {
+      window.location.replace("win.html");
+   }
+   else
+   {
+      wrong.textContent="wrong password and user name"
+   }
+}
